db53ec8670b2c634308010f4b07ae2f7ab608002
ginmanzzz <595348563@qq.com>
2024-12-07 03:03:44 +0800

    Added proxy

--- build_docker.sh
+++ build_docker.sh
@@ -1,12 +1,16 @@
 # !/bin/bash
 
-HOST_WORK_DIR="$HOME/work_dir"
+HOST_WORK_DIR="$HOME/dockerworkdir"
 WORK_DIR="/app"
 IMG_NAME="ubuntu_img"
 CON_NAME="ubuntu_con"
 
-# 1. Build image
+# 1. Proxy the docker pull request
+export HTTP_PROXY=http://localhost:7890
+export HTTPS_PROXY=https://localhost:7890
+
+# 2. Build image
 docker build -t $IMG_NAME .
 
-# 2. Build container
+# 3. Build container
 docker run -it -v $HOST_WORK_DIR:$WORK_DIR --name $CON_NAME $IMG_NAME
